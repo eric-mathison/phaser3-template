20882bc02a353a5c34a2bfa416546b5ec2913698
Eric Mathison <7695760+eric-mathison@users.noreply.github.com>
2019-07-29 22:27:33 -0500

    add progress loader and fadein

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import Phaser from 'phaser';
 import { SimpleScene } from './scenes/simple-scene';
 
 const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  scene: SimpleScene
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: SimpleScene,
+    parent: 'phaser-app'
 };
 
-new Phaser.Game(config);
+const game = new Phaser.Game(config);
