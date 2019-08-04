632f1d2805e25c6d3446754369e9917a93d54642
Eric Mathison <7695760+eric-mathison@users.noreply.github.com>
2019-08-04 08:12:29 -0500

    moved preloader to seperate scene

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import Phaser from 'phaser';
-import { SimpleScene } from './scenes/simple-scene';
+import { SceneLoader } from './scenes/SceneLoader';
+import { SceneMain } from './scenes/SceneMain';
 
 const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: SimpleScene,
+    scene: [
+        SceneLoader,
+        SceneMain
+    ],
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
     parent: 'phaser-app'
 };
 
